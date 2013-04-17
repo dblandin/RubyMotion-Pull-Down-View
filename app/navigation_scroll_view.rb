b1317401074ce565cc8b8685b04b43a06b7b9803
Devon Blandin <dblandin@gmail.com>
2013-04-17 13:13:10 -0500

    Remove magic numbers. Use animateWithDuration:animations:.

--- app/navigation_scroll_view.rb
+++ app/navigation_scroll_view.rb
@@ -1,69 +1,76 @@
 class NavigationScrollView < UIScrollView
-  attr_accessor :starting_y_position, :parent_view
+  attr_accessor :starting_y_position, :parent
+
+  NAVIGATION_HEIGHT  = 300
+  BUTTON_SIDE_LENGTH = 100
 
   def initWithFrame(frame)
     if super
       self.delegate        = self
       self.bounces         = false
       self.scrollsToTop    = false
-      self.contentSize     = CGSizeMake(size.width, size.height + 300)
-      self.contentOffset   = CGPointMake(0, 300)
+      self.contentSize     = CGSizeMake(scroll_view_width, scroll_view_height + NAVIGATION_HEIGHT)
+      self.contentOffset   = CGPointMake(0, NAVIGATION_HEIGHT)
       self.backgroundColor = UIColor.clearColor
       self.showsVerticalScrollIndicator = false
     end
 
-    self
-  end
-
-  def layoutSubviews
     addSubview(navigation)
     addSubview(label)
     addSubview(handle)
+
+    self
   end
 
   def hitTest(point, withEvent: event)
-    super if CGRectContainsPoint(handle.frame, point)
-  end
-
-  def label
-    @_label ||= UILabel.alloc.initWithFrame(CGRectMake(0, 0, 200, 60)).tap do |label|
-      label.textColor = UIColor.blackColor
-      label.center = CGPointMake(size.width / 2, size.height / 2)
-      label.textAlignment = UITextAlignmentCenter
-      label.text = 'Navigation'
-    end
-  end
-
-  def handle
-    @_handle ||= UIView.alloc.initWithFrame(CGRectMake(0, 0, 100 , 100)).tap do |handle|
-      handle.center = CGPointMake(0, 300)
-      handle.transform = CGAffineTransformMakeRotation(Math.degrees_to_radians(45))
-      handle.backgroundColor = UIColor.blackColor
-    end
-  end
-
-  def navigation
-    @_navigation ||= UIView.alloc.initWithFrame(CGRectMake(0, 0, size.width, 300)).tap do |nav|
-      nav.backgroundColor = UIColor.blackColor
-    end
+    super if CGRectContainsPoint(handle.frame, point) || CGRectContainsPoint(navigation.frame, point)
   end
 
   def scrollViewWillBeginDragging(scroll_view)
     update_starting_y_position(contentOffset.y)
   end
 
-  def update_starting_y_position(y_position)
-    self.starting_y_position = y_position
-  end
-
   def scrollViewDidEndDragging(scroll_view, willDecelerate: decelerate)
     if navigation_moved_down?
       move_navigation_down
-      dim_superview
     elsif navigation_moved_up?
       move_navigation_up
-      undim_superview
     end
+  end
+
+  def navigation
+    @_navigation ||= UIView.alloc.initWithFrame(CGRectMake(0, 0, size.width, NAVIGATION_HEIGHT)).tap do |nav|
+      nav.backgroundColor = UIColor.blackColor
+    end
+  end
+
+  def handle
+    @_handle ||= UIView.alloc.initWithFrame(CGRectMake(0, 0, BUTTON_SIDE_LENGTH , BUTTON_SIDE_LENGTH)).tap do |handle|
+      handle.center          = CGPointMake(0, NAVIGATION_HEIGHT)
+      handle.transform       = CGAffineTransformMakeRotation(Math.degrees_to_radians(45))
+      handle.backgroundColor = UIColor.blackColor
+    end
+  end
+
+  def label
+    @_label ||= UILabel.alloc.initWithFrame(CGRectMake(0, 0, 200, 60)).tap do |label|
+      label.text          = 'Navigation'
+      label.textColor     = UIColor.blackColor
+      label.center        = CGPointMake(scroll_view_width / 2, NAVIGATION_HEIGHT / 2)
+      label.textAlignment = UITextAlignmentCenter
+    end
+  end
+
+  def scroll_view_height
+    size.height
+  end
+
+  def scroll_view_width
+    size.width
+  end
+
+  def update_starting_y_position(y_position)
+    self.starting_y_position = y_position
   end
 
   def navigation_moved_down?
@@ -72,6 +79,14 @@ class NavigationScrollView < UIScrollView
 
   def navigation_moved_up?
     starting_y_position < contentOffset.y
+  end
+
+  def navigation_is_down?
+    contentOffset.y === 0
+  end
+
+  def navigation_is_up?
+    contentOffset.y === NAVIGATION_HEIGHT
   end
 
   def dim_superview
@@ -83,14 +98,17 @@ class NavigationScrollView < UIScrollView
   end
 
   def animate_parent_background_color(color)
-    UIView.beginAnimations(nil, context:nil)
-    UIView.setAnimationDuration(0.5)
-    parent_view.view.backgroundColor = color
-    UIView.commitAnimations
+    UIView.animateWithDuration(0.5, animations: lambda {
+      parent_view.backgroundColor = color if parent
+    })
+  end
+
+  def parent_view
+    parent.view
   end
 
   def move_navigation_up
-    move_to_offset(CGPointMake(0, 300))
+    move_to_offset(CGPointMake(0, NAVIGATION_HEIGHT))
   end
 
   def move_navigation_down
@@ -98,10 +116,9 @@ class NavigationScrollView < UIScrollView
   end
 
   def move_to_offset(offset)
-    UIView.beginAnimations(nil, context:nil)
-    UIView.setAnimationCurve(UIViewAnimationCurveEaseOut)
-    UIView.setAnimationDuration(0.5)
-    self.contentOffset = offset
-    UIView.commitAnimations
+    UIView.animateWithDuration(0.5, animations: lambda {
+      UIView.setAnimationCurve(UIViewAnimationCurveEaseOut)
+      self.contentOffset = offset
+    })
   end
 end
--- app/view_controller.rb
+++ app/view_controller.rb
@@ -1,19 +1,33 @@
 class ViewController < UIViewController
+  BUTTON_SIDE_LENGTH = 100
+
   def viewDidLoad
     super
 
     view.backgroundColor = UIColor.whiteColor
 
-    NavigationScrollView.alloc.initWithFrame(viewport_size).tap do |nav_view|
-      nav_view.parent_view = self
-      view.insertSubview(nav_view, atIndex: 100)
-    end
+    view.addSubview(button)
 
-    view.insertSubview(button, atIndex: 0)
+    NavigationScrollView.alloc.initWithFrame(viewport_size).tap do |nav_view|
+      nav_view.parent = self
+      view.addSubview(nav_view)
+    end
   end
 
-  def viewport_size
-    CGRectMake(0, 0, view.size.width, view.size.height)
+  def enable_controls
+    controls_to_disable.each do |control|
+      control.enabled = true
+    end
+  end
+
+  def disable_controls
+    controls_to_disable.each do |control|
+      control.enabled = false
+    end
+  end
+
+  def controls_to_disable
+    [button]
   end
 
   def button_pressed(sender)
@@ -26,11 +40,23 @@ class ViewController < UIViewController
   end
 
   def button
-    UIButton.alloc.initWithFrame(CGRectMake(0, 0, 100 , 100)).tap do |button|
-      button.center          = CGPointMake(view.size.width, view.size.height)
+    UIButton.alloc.initWithFrame(CGRectMake(0, 0, BUTTON_SIDE_LENGTH , BUTTON_SIDE_LENGTH)).tap do |button|
+      button.center          = CGPointMake(viewport_width, viewport_height)
       button.transform       = CGAffineTransformMakeRotation(Math.degrees_to_radians(45))
       button.backgroundColor = UIColor.blackColor
       button.addTarget(self, action: 'button_pressed:', forControlEvents: UIControlEventTouchUpInside)
     end
+  end
+
+  def viewport_size
+    CGRectMake(0, 0, viewport_width, viewport_height)
+  end
+
+  def viewport_width
+    view.size.width
+  end
+
+  def viewport_height
+    view.size.height
   end
 end
